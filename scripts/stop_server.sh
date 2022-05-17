dc95ca35cc609044ed9b3bab5ae428ea26fc94ef
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-17 10:52:54 +0300

    gunicorn stop in stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
+
+
+pid = `ps ax | grep gunicorn`
+
+if [ -z "$pid" ]; then
+  echo "no gunicorn deamon on port $Port"
+else
+  kill $pid
+fi
