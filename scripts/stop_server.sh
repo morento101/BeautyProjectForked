053215ad30e3db938e4208b1b4345e1017ecd18c
Misha86 <mpolishchuk86@gmail.com>
2022-07-15 08:46:54 +0300

    Changed start_server

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
-
 supervisorctl -u user -p 123 shutdown
+
+if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
 
 sudo pkill -P1 gunicorn
