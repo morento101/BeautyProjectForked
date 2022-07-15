e7d800d14a1072ef0c57264dbdee7955c781f33b
Misha86 <mpolishchuk86@gmail.com>
2022-07-15 10:33:28 +0300

    Changed stop_server

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e  /tmp/supervisord.pid ]; then sudo pkill -F /tmp/supervisord.pid; fi
+if [ -e  /tmp/supervisord.pid ]; then sudo supervisorctl shutdown; fi
 
 if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
 
