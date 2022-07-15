d7cf4f91a36bdc79e5aa3ad1b2e6480dfa423ec6
Misha86 <mpolishchuk86@gmail.com>
2022-07-15 11:56:16 +0300

    Changed supervisord.conf

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e  /tmp/supervisord.pid ]; then sudo kill -9 $(lsof -t -i:9001); fi
+if [ -e  /tmp/supervisord.pid ]; then sudo supervisorctl -u user -p 123 shutdown; fi
 
 if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
 
