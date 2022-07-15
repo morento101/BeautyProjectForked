6f27861e39f735658fe2bb470daa7331455a38c5
Misha86 <mpolishchuk86@gmail.com>
2022-07-15 11:04:48 +0300

    Changed programs.conf

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -e  /tmp/supervisord.pid ]; then sudo supervisorctl shutdown; fi
+if [ -e  /tmp/supervisord.pid ]; then sudo supervisorctl -u user -p 123 shutdown; fi
 
 if [ -e /var/run/nginx.pid ]; then sudo nginx -s stop; fi
 
